b236a19264e2b7b792abde2b0a731ed236e1be97
Kumar Gaurav <48727152+kgaurav123@users.noreply.github.com>
2020-04-15 22:51:59 +0530

    home page added

--- ecommerce/home/models.py
+++ ecommerce/home/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
+    def __str__(self):
+        return self.product_name
+    
